d42b4337cd1599f4f01f0a5d1f37982e2dd6370b
Gabriel Ramirez <gxb170@gmail.com>
2020-06-24 15:50:54 -0600

    Base uri font

--- urlBase.js
+++ urlBase.js
@@ -1,6 +1,6 @@
 const baseUri = 'https://restaurantebk.herokuapp.com/restauranteBK/tpi_restaurante/';
 //const baseUri = 'http://localhost:3000/api';
-const baseUriFront = 'http://127.0.0.1:5500'
+const baseUriFront = 'https://gabriel-ramirez.github.io/restauranteTPI'
 
 function cerrarSesion(){
     localStorage.removeItem('token');
