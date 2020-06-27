b8c7604a41e78f75c5b3431555f09d074206fb4a
Cristian Ayala <cristianaaron10@gmail.com>
2020-06-26 18:21:41 -0600

    router added

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,32 +1,29 @@
-
-$('tr').on('dblclick', function() {
+$('tr').on('dblclick', function () {
     $('#addModal').modal('show');
 });
 
 
 $('#guasap').scrollspy();
-  
-    //Cuando el modal se muestre
-$('#modalNuevaOrden').on('show.bs.modal', function() {
-console.log("me mostré");
-$( "#generales" ).addClass( "active" );
-$('#asaber').scrollspy();
-    });
 
-    //Cuando el modal se cierre
-$('#modalNuevaOrden').on('hide.bs.modal', function() {
-        console.log("me escondí");
-   $( "#ordenDelNav" ).addClass( "active" );
-    });
+//Cuando el modal se muestre
+$('#modalNuevaOrden').on('show.bs.modal', function () {
+    console.log("me mostré");
+    $("#generales").addClass("active");
+    $('#asaber').scrollspy();
+});
+
+//Cuando el modal se cierre
+$('#modalNuevaOrden').on('hide.bs.modal', function () {
+    console.log("me escondí");
+    $("#ordenDelNav").addClass("active");
+});
 
 
 //te da el número de pixeles donde se encuentra el scroll
-function onScrollEvent(){
+function onScrollEvent() {
     var elmnt = document.getElementById("guasap").scrollTop;
     //35 px
-    if(elmnt<34){
-        $( "#generales" ).addClass( "active" );
+    if (elmnt < 34) {
+        $("#generales").addClass("active");
     }
 }
-
-
--- urlBase.js
+++ urlBase.js
@@ -4,12 +4,44 @@ const baseUri = 'https://restaurantebk.herokuapp.com/restauranteBK/tpi_restauran
 const baseUriFront = 'http://localhost:5500';
 
 const headers = {
-    'Content-Type':'application/json;charset=utf-8',
+    'Content-Type': 'application/json;charset=utf-8',
     'JWT': localStorage.getItem('token'),
     "Access-Control-Allow-Headers": "Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers, jwt, mensaje"
 }
 
-function cerrarSesion(){
+function cerrarSesion() {
     localStorage.removeItem('token');
     window.location = "./login.html";
 };
+
+
+//chequear el token
+function checkJWT() {
+    if (location.pathname === "/login.html") {} else {
+        if (localStorage.getItem('token') === null) {
+            //Verifica si estamos en github pages o en local
+            if (location.origin === 'https://gabriel-ramirez.github.io') {
+                //Si estamos en el repo
+                location.href = location.origin + "/restauranteTPI/login.html";
+            } else {
+                //Si estamos en local
+                location.href = location.origin + "/login.html";
+            }
+        } else {
+            //Si existe el token y verifica que sea uno valido
+            axios.get(baseUri + '/usuario/checkJWT/'+localStorage.getItem('token'))
+                .then(response => {
+                    console.log("JWT: ","valido");
+                })
+                .catch(function (error) {
+                    console.log(error);
+                    localStorage.removeItem("token");
+                    checkJWT();
+                });
+        }
+    }
+}
+
+window.addEventListener('DOMContentLoaded', (event) => {
+    checkJWT();
+});
